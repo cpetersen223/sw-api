87b0fd760b298f258145de732c618c13ea5f6236
Christian Petersen <christianpetersen.ve@gmail.com>
2019-01-07 01:18:47 -0300

    Adds some seeds for development purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  p '--- Creating new author ---'
+  author = Author.create(name: Faker::RickAndMorty.character,
+                         email: Faker::Internet.safe_email,
+                         birth_date: Faker::Date.birthday)
+  10.times do
+    p "--- Creating publications of author: #{author.name} ---"
+    author.publications.create(title: Faker::Book.title,
+                               body: Faker::Lorem.paragraph,
+                               date: Faker::Date.between(Date.today - 65.years, Date.today),
+                               time: Faker::Time.backward)
+  end
+end
