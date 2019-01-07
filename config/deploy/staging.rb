49acfedd46e00bfda2d8279681602ff4aa75ff96
Christian Petersen <christianpetersen.ve@gmail.com>
2019-01-07 01:02:09 -0300

    Staging deployment config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,25 +3,16 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+server ENV['DEPLOY_HOST'],
+       user: ENV['DEPLOY_USER'],
+       roles: %w[web app db],
+       password: ENV['DEPLOY_PASS']
 
+set :branch, :master
+set :stage, :staging
+set :rails_env, :staging
 
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
+# Capistrano Deployment Route
 
 # Configuration
 # =============
@@ -59,3 +50,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+# Server definition: (define ip in local env, and pass in gitlab)
